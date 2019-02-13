04e988566628a1809a33d4a5d10f6e4af9e21c8a
AndGate92 <mrandre.epfl@gmail.com>
2019-02-13 23:18:40 +0000

    Add window_list.h with WindowList class declaration

---
+++ include/window_list.h
@@ -0,0 +1,39 @@
+#ifndef WINDOW_LIST_H
+#define WINDOW_LIST_H
+/**
+ * @copyright
+ * @file window_list.h
+ * @author Andrea Gianarda
+ * @date 12th of February 2019
+ * @brief Window List header file
+*/
+
+#include "window_obj.h"
+
+namespace window_list {
+	/** @defgroup WindowListGroup Window List Doxygen Group
+	 *  Window List functions and classes
+	 *  @{
+	 */
+
+	template <typename entry_e>
+	class WindowList {
+		public:
+			// Constructor
+			WindowList(std::string window_title = "", int window_width = 0, int window_height = 0, int window_x_pos = WIN_POS_X, int window_y_pos = WIN_POS_Y, void (&EntryFunc)(entry_e) = nullptr, void (*ItemsFunc)() = nullptr);
+
+			// Destructor
+			~WindowList();
+
+		protected:
+
+		private:
+			WindowList * prev;
+			window_obj::WindowObj<entry_e> node;
+			WindowList *next;
+	};
+	/** @} */ // End of WindowListGroup group
+}
+
+
+#endif // WINDOW_LIST_H
