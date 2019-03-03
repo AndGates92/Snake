39e18ab8b221e01a107b7e482564a68a1b3ecc6a
AndGate92 <mrandre.epfl@gmail.com>
2019-03-03 19:51:33 +0000

    Set RGB elements of colours in the palette

--- include/graphics_utils.h
+++ include/graphics_utils.h
@@ -73,12 +73,15 @@ pixel_type * graphics_utils::get_pixel_colour (graphics_utils::palette_e colour_
 	static pixel_type max_value_pixel_type = std::numeric_limits<pixel_type>::max();
 	static pixel_type min_value_pixel_type = std::numeric_limits<pixel_type>::min();
 
+	static unsigned char max_value_uchar = std::numeric_limits<unsigned char>::max();
+	static unsigned char min_value_uchar = std::numeric_limits<unsigned char>::min();
+
 	switch (colour_name) {
 		case graphics_utils::palette_e::RED:
 			if (graphics_utils::no_colours == 3) {
 				colour[(int)graphics_utils::colour_idx_e::RED] =  max_value_pixel_type;
-				colour[(int)graphics_utils::colour_idx_e::BLUE] =  min_value_pixel_type;
 				colour[(int)graphics_utils::colour_idx_e::GREEN] =  min_value_pixel_type;
+				colour[(int)graphics_utils::colour_idx_e::BLUE] =  min_value_pixel_type;
 			} else if (graphics_utils::no_colours == 1) {
 				colour[0] =  max_value_pixel_type;
 			}
@@ -86,8 +89,8 @@ pixel_type * graphics_utils::get_pixel_colour (graphics_utils::palette_e colour_
 		case graphics_utils::palette_e::BLUE:
 			if (graphics_utils::no_colours == 3) {
 				colour[(int)graphics_utils::colour_idx_e::RED] =  min_value_pixel_type;
-				colour[(int)graphics_utils::colour_idx_e::BLUE] =  max_value_pixel_type;
 				colour[(int)graphics_utils::colour_idx_e::GREEN] =  min_value_pixel_type;
+				colour[(int)graphics_utils::colour_idx_e::BLUE] =  max_value_pixel_type;
 			} else if (graphics_utils::no_colours == 1) {
 				colour[0] =  max_value_pixel_type;
 			}
@@ -95,8 +98,8 @@ pixel_type * graphics_utils::get_pixel_colour (graphics_utils::palette_e colour_
 		case graphics_utils::palette_e::GREEN:
 			if (graphics_utils::no_colours == 3) {
 				colour[(int)graphics_utils::colour_idx_e::RED] =  min_value_pixel_type;
-				colour[(int)graphics_utils::colour_idx_e::BLUE] =  min_value_pixel_type;
 				colour[(int)graphics_utils::colour_idx_e::GREEN] =  max_value_pixel_type;
+				colour[(int)graphics_utils::colour_idx_e::BLUE] =  min_value_pixel_type;
 			} else if (graphics_utils::no_colours == 1) {
 				colour[0] =  max_value_pixel_type;
 			}
@@ -104,35 +107,35 @@ pixel_type * graphics_utils::get_pixel_colour (graphics_utils::palette_e colour_
 		case graphics_utils::palette_e::YELLOW:
 			if (graphics_utils::no_colours == 3) {
 				colour[(int)graphics_utils::colour_idx_e::RED] =  max_value_pixel_type;
+				colour[(int)graphics_utils::colour_idx_e::GREEN] =  max_value_pixel_type;
 				colour[(int)graphics_utils::colour_idx_e::BLUE] =  min_value_pixel_type;
-				colour[(int)graphics_utils::colour_idx_e::GREEN] =  min_value_pixel_type;
 			} else if (graphics_utils::no_colours == 1) {
 				colour[0] =  max_value_pixel_type;
 			}
 			break;
 		case graphics_utils::palette_e::PURPLE:
 			if (graphics_utils::no_colours == 3) {
-				colour[(int)graphics_utils::colour_idx_e::RED] =  min_value_pixel_type;
-				colour[(int)graphics_utils::colour_idx_e::BLUE] =  max_value_pixel_type;
+				colour[(int)graphics_utils::colour_idx_e::RED] =  (pixel_type) (((128 / ((float)max_value_uchar - (float)min_value_uchar)) * (float)max_value_pixel_type) + (float)min_value_pixel_type);
 				colour[(int)graphics_utils::colour_idx_e::GREEN] =  min_value_pixel_type;
+				colour[(int)graphics_utils::colour_idx_e::BLUE] =  (pixel_type) (((128 / ((float)max_value_uchar - (float)min_value_uchar)) * (float)max_value_pixel_type) + (float)min_value_pixel_type);
 			} else if (graphics_utils::no_colours == 1) {
 				colour[0] =  max_value_pixel_type;
 			}
 			break;
 		case graphics_utils::palette_e::PINK:
 			if (graphics_utils::no_colours == 3) {
-				colour[(int)graphics_utils::colour_idx_e::RED] =  min_value_pixel_type;
-				colour[(int)graphics_utils::colour_idx_e::BLUE] =  min_value_pixel_type;
-				colour[(int)graphics_utils::colour_idx_e::GREEN] =  max_value_pixel_type;
+				colour[(int)graphics_utils::colour_idx_e::RED] =  max_value_pixel_type;
+				colour[(int)graphics_utils::colour_idx_e::GREEN] =  (pixel_type) (((192 / ((float)max_value_uchar- (float)min_value_uchar)) * (float)max_value_pixel_type) + (float)min_value_pixel_type);
+				colour[(int)graphics_utils::colour_idx_e::BLUE] =  (pixel_type) (((203 / ((float)max_value_uchar - (float)min_value_uchar)) * (float)max_value_pixel_type) + (float)min_value_pixel_type);
 			} else if (graphics_utils::no_colours == 1) {
 				colour[0] =  max_value_pixel_type;
 			}
 			break;
 		case graphics_utils::palette_e::BROWN:
 			if (graphics_utils::no_colours == 3) {
-				colour[(int)graphics_utils::colour_idx_e::RED] =  max_value_pixel_type;
-				colour[(int)graphics_utils::colour_idx_e::BLUE] =  min_value_pixel_type;
-				colour[(int)graphics_utils::colour_idx_e::GREEN] =  min_value_pixel_type;
+				colour[(int)graphics_utils::colour_idx_e::RED] =  (pixel_type) (((165 / ((float)max_value_uchar- (float)min_value_uchar)) * (float)max_value_pixel_type) + (float)min_value_pixel_type);
+				colour[(int)graphics_utils::colour_idx_e::GREEN] =  (pixel_type) (((42 / ((float)max_value_uchar- (float)min_value_uchar)) * (float)max_value_pixel_type) + (float)min_value_pixel_type);
+				colour[(int)graphics_utils::colour_idx_e::BLUE] =  (pixel_type) (((42 / ((float)max_value_uchar - (float)min_value_uchar)) * (float)max_value_pixel_type) + (float)min_value_pixel_type);
 			} else if (graphics_utils::no_colours == 1) {
 				colour[0] =  max_value_pixel_type;
 			}
@@ -140,8 +143,8 @@ pixel_type * graphics_utils::get_pixel_colour (graphics_utils::palette_e colour_
 		case graphics_utils::palette_e::WHITE:
 			if (graphics_utils::no_colours == 3) {
 				colour[(int)graphics_utils::colour_idx_e::RED] =  max_value_pixel_type;
-				colour[(int)graphics_utils::colour_idx_e::BLUE] =  max_value_pixel_type;
 				colour[(int)graphics_utils::colour_idx_e::GREEN] =  max_value_pixel_type;
+				colour[(int)graphics_utils::colour_idx_e::BLUE] =  max_value_pixel_type;
 			} else if (graphics_utils::no_colours == 1) {
 				colour[0] =  max_value_pixel_type;
 			}
@@ -149,8 +152,8 @@ pixel_type * graphics_utils::get_pixel_colour (graphics_utils::palette_e colour_
 		case graphics_utils::palette_e::BLACK:
 			if (graphics_utils::no_colours == 3) {
 				colour[(int)graphics_utils::colour_idx_e::RED] =  min_value_pixel_type;
-				colour[(int)graphics_utils::colour_idx_e::BLUE] =  min_value_pixel_type;
 				colour[(int)graphics_utils::colour_idx_e::GREEN] =  min_value_pixel_type;
+				colour[(int)graphics_utils::colour_idx_e::BLUE] =  min_value_pixel_type;
 			} else if (graphics_utils::no_colours == 1) {
 				colour[0] =  max_value_pixel_type;
 			}
