806892e0de07980958c5d75479d6eb596467d75a
Andrea Gianarda <mrandre.epfl@gmail.com>
2019-03-05 22:51:02 +0000

    Rename snake_unit to snake_node

--- include/snake_node.h
+++ include/snake_node.h
@@ -1,19 +1,19 @@
-#ifndef SNAKE_UNIT_H
-#define SNAKE_UNIT_H
+#ifndef SNAKE_NODE_H
+#define SNAKE_NODE_H
 /**
  * @copyright
- * @file snake_unit.h
+ * @file snake_node.h
  * @author Andrea Gianarda
  * @date 03rd of March 2019
- * @brief Snake unit header file
+ * @brief Snake node header file
 */
 
 #include "graphics_utils.h"
 
-namespace snake_unit {
+namespace snake_node {
 
-	/** @defgroup SnakeUnitGroup Snake Unit Doxygen Group
-	 *  Snake Unit functions and classes
+	/** @defgroup SnakeNodeGroup Snake Node Doxygen Group
+	 *  Snake Node functions and classes
 	 *  @{
 	 */
 
@@ -34,32 +34,32 @@ namespace snake_unit {
 	 * @brief Initial speed
 	 *
 	 */
-	const static snake_unit::direction_e init_direction = snake_unit::direction_e::RIGHT;
+	const static snake_node::direction_e init_direction = snake_node::direction_e::RIGHT;
 
 
 	class SnakeUnit {
 		public:
 			// Constructor
-			SnakeUnit(int centre_x = 0, int centre_y = 0, snake_unit::direction_e snake_direction = snake_unit::init_direction, graphics_utils::palette_e snake_colour = graphics_utils::palette_e::RED): x_centre(centre_x), y_centre(centre_y), direction(snake_direction), colour(snake_colour) {
+			SnakeUnit(int centre_x = 0, int centre_y = 0, snake_node::direction_e snake_direction = snake_node::init_direction, graphics_utils::palette_e snake_colour = graphics_utils::palette_e::RED): x_centre(centre_x), y_centre(centre_y), direction(snake_direction), colour(snake_colour) {
 				std::string pretext ("Snake Unit Constructor");
-				snake_unit::SnakeUnit::print_info(log::verb_level_e::LOW, pretext);
+				snake_node::SnakeUnit::print_info(log::verb_level_e::LOW, pretext);
 			};
 
 			SnakeUnit(const SnakeUnit& copy) : x_centre(copy.x_centre), y_centre(copy.y_centre), direction(copy.direction), colour(copy.colour) {
 				std::string pretext ("Snake Unit Copy Constructor");
-				snake_unit::SnakeUnit::print_info(log::verb_level_e::LOW, pretext);
+				snake_node::SnakeUnit::print_info(log::verb_level_e::LOW, pretext);
 			};
 
 			// Get functions
 			int get_x_centre();
 			int get_y_centre();
-			snake_unit::direction_e get_direction();
+			snake_node::direction_e get_direction();
 			graphics_utils::palette_e get_colour();
 
 			// Set functions
 			void set_x_centre(int new_x_centre);
 			void set_y_centre(int new_y_centre);
-			void set_direction(snake_unit::direction_e new_direction);
+			void set_direction(snake_node::direction_e new_direction);
 			void set_colour(graphics_utils::palette_e new_colour);
 
 			// Destructor
@@ -70,7 +70,7 @@ namespace snake_unit {
 		private:
 			int x_centre;
 			int y_centre;
-			snake_unit::direction_e direction;
+			snake_node::direction_e direction;
 			graphics_utils::palette_e colour;
 
 		protected:
@@ -79,9 +79,9 @@ namespace snake_unit {
 	class SnakeNode : public SnakeUnit {
 		public:
 			// Constructor
-			SnakeNode(int centre_x = 0, int centre_y = 0, snake_unit::direction_e snake_direction = snake_unit::init_direction, graphics_utils::palette_e snake_colour = graphics_utils::palette_e::RED): SnakeUnit(centre_x, centre_y, snake_direction, snake_colour), prev(nullptr), next(nullptr) {
+			SnakeNode(int centre_x = 0, int centre_y = 0, snake_node::direction_e snake_direction = snake_node::init_direction, graphics_utils::palette_e snake_colour = graphics_utils::palette_e::RED): SnakeUnit(centre_x, centre_y, snake_direction, snake_colour), prev(nullptr), next(nullptr) {
 				std::string pretext ("Snake Node Constructor");
-				snake_unit::SnakeNode::print_info(log::verb_level_e::LOW, pretext);
+				snake_node::SnakeNode::print_info(log::verb_level_e::LOW, pretext);
 			};
 
 			SnakeNode(const SnakeNode& copy) : SnakeUnit(copy), prev(copy.prev), next(copy.next) { LOG_INFO(log::verb_level_e::LOW, "Snake node copy contructor") };
@@ -105,7 +105,7 @@ namespace snake_unit {
 			SnakeNode * prev;
 			SnakeNode * next;
 	};
-	/** @} */ // End of SnakeUnitGroup group
+	/** @} */ // End of SnakeNodeGroup group
 }
 
-#endif // SNAKE_UNIT_H
+#endif // SNAKE_NODE_H
--- src/snake_node.cpp
+++ src/snake_node.cpp
@@ -1,6 +1,6 @@
 /**
  * @copyright
- * @file snake_unit.cpp
+ * @file snake_node.cpp
  * @author Andrea Gianarda
  * @date 04th March 2019
  * @brief Snake Unit function
@@ -8,79 +8,79 @@
 
 #include <iostream>
 
-#include "snake_unit.h"
+#include "snake_node.h"
 #include "log.h"
 
 using namespace std;
-using namespace snake_unit;
+using namespace snake_node;
 using namespace log;
 
 // ================================================================
 // Destructor
 // ================================================================
-snake_unit::SnakeUnit::~SnakeUnit() {
+snake_node::SnakeUnit::~SnakeUnit() {
 	std::string pretext ("Window Destructor");
 	this->print_info(log::verb_level_e::LOW, pretext);
 }
 
-void snake_unit::SnakeUnit::print_info(log::verb_level_e verbosity, std::string pretext) {
+void snake_node::SnakeUnit::print_info(log::verb_level_e verbosity, std::string pretext) {
 	LOG_INFO(verbosity, "[", pretext, "] Centre coordinares: (X ", this->x_centre, ", Y ", this->y_centre, "), direction ", this->direction, " colour ", this->colour, ".");
 }
 
 // ================================================================
 // Get functions
 // ================================================================
-int snake_unit::SnakeUnit::get_x_centre() {
+int snake_node::SnakeUnit::get_x_centre() {
 	return x_centre;
 }
 
-int snake_unit::SnakeUnit::get_y_centre() {
+int snake_node::SnakeUnit::get_y_centre() {
 	return y_centre;
 }
 
-snake_unit::direction_e snake_unit::SnakeUnit::get_direction() {
+snake_node::direction_e snake_node::SnakeUnit::get_direction() {
 	return direction;
 }
 
-graphics_utils::palette_e snake_unit::SnakeUnit::get_colour() {
+graphics_utils::palette_e snake_node::SnakeUnit::get_colour() {
 	return colour;
 }
 
 // ================================================================
 // Set functions
 // ================================================================
-void snake_unit::SnakeUnit::set_x_centre(int new_x_centre) {
+void snake_node::SnakeUnit::set_x_centre(int new_x_centre) {
 	this->x_centre = new_x_centre;
 }
 
-void snake_unit::SnakeUnit::set_y_centre(int new_y_centre) {
+void snake_node::SnakeUnit::set_y_centre(int new_y_centre) {
 	this->y_centre = new_y_centre;
 }
 
-void snake_unit::SnakeUnit::set_direction(snake_unit::direction_e new_direction) {
+void snake_node::SnakeUnit::set_direction(snake_node::direction_e new_direction) {
 	this->direction = new_direction;
 }
 
-void snake_unit::SnakeUnit::set_colour(graphics_utils::palette_e new_colour) {
+void snake_node::SnakeUnit::set_colour(graphics_utils::palette_e new_colour) {
 	this->colour = new_colour;
 }
 
 // ================================================================
 // Overload << operator for direction_e
 // ================================================================
-std::ostream& snake_unit::operator<< (std::ostream& os, snake_unit::direction_e direction) {
+std::ostream& snake_node::operator<< (std::ostream& os, snake_node::direction_e direction) {
 
 	switch (direction) {
-		case snake_unit::direction_e::UP:
+		case snake_node::direction_e::UP:
 			os << "UP";
 			break;
-		case snake_unit::direction_e::DOWN:
+		case snake_node::direction_e::DOWN:
 			os << "DOWN";
 			break;
-		case snake_unit::direction_e::LEFT:
+		case snake_node::direction_e::LEFT:
 			os << "LEFT";
 			break;
-		case snake_unit::direction_e::RIGHT:
+		case snake_node::direction_e::RIGHT:
 			os << "RIGHT";
 			break;
 		default:
@@ -94,7 +94,7 @@ std::ostream& snake_unit::operator<< (std::ostream& os, snake_unit::direction_e 
 // ================================================================
 // Snake Node
 // ================================================================
-snake_unit::SnakeNode::~SnakeNode() {
+snake_node::SnakeNode::~SnakeNode() {
 	std::string pretext ("Snake Node Destructor");
 	this->print_info(log::verb_level_e::LOW, pretext);
 }
@@ -103,12 +103,12 @@ snake_unit::SnakeNode::~SnakeNode() {
 // Get functions
 // ================================================================
 
-snake_unit::SnakeNode * & snake_unit::SnakeNode::get_next() {
+snake_node::SnakeNode * & snake_node::SnakeNode::get_next() {
 	LOG_INFO(log::verb_level_e::DEBUG, "Get pointer next: ", this->next);
 	return this->next;
 }
 
-snake_unit::SnakeNode * & snake_unit::SnakeNode::get_prev() {
+snake_node::SnakeNode * & snake_node::SnakeNode::get_prev() {
 	LOG_INFO(log::verb_level_e::DEBUG, "Get pointer prev: ", this->prev);
 	return this->prev;
 }
@@ -117,15 +117,15 @@ snake_unit::SnakeNode * & snake_unit::SnakeNode::get_prev() {
 // Set functions
 // ================================================================
 
-void snake_unit::SnakeNode::set_next(snake_unit::SnakeNode * next_ptr) {
+void snake_node::SnakeNode::set_next(snake_node::SnakeNode * next_ptr) {
 	this->next = next_ptr;
 }
 
-void snake_unit::SnakeNode::set_prev(snake_unit::SnakeNode * prev_ptr) {
+void snake_node::SnakeNode::set_prev(snake_node::SnakeNode * prev_ptr) {
 	this->prev = prev_ptr;
 }
 
-void snake_unit::SnakeNode::print_info(log::verb_level_e verbosity, std::string pretext) {
+void snake_node::SnakeNode::print_info(log::verb_level_e verbosity, std::string pretext) {
 	SnakeUnit::print_info(verbosity, pretext);
 	if (this->prev != nullptr) {
 		this->prev->print_info(verbosity, pretext);
