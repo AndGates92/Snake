2d4e1c8b33e0651e797018858883aa68c6bea391
Andrea Gianarda <mrandre.epfl@gmail.com>
2019-03-21 22:49:43 +0000

    Add logic to initialize snake vertically and horizontally

--- src/game_graphics.cpp
+++ src/game_graphics.cpp
@@ -229,10 +229,11 @@ void game_graphics::populate_snake_list() {
 	int centre_y = 100;
 	for (int unit_no=0; unit_no < game_graphics::init_units; unit_no++) {
 
-//		game_graphics::snake->add_node(centre_x, centre_y, snake_node_width, snake_node_height, snake_node::direction_e::RIGHT, (graphics_utils::palette_e)unit_no);
-		game_graphics::snake->add_node(centre_x, centre_y, game_graphics::snake_node_width, game_graphics::snake_node_height, snake_node::direction_e::RIGHT, graphics_utils::palette_e::RED);
 
-		centre_x += snake_node_width;
+		game_graphics::snake->add_node(centre_x, centre_y, game_graphics::snake_node_width, game_graphics::snake_node_height, snake_node::direction_e::RIGHT, graphics_utils::palette_e::GREEN);
+		centre_x += game_graphics::snake_node_width;
+		//game_graphics::snake->add_node(centre_x, centre_y, game_graphics::snake_node_width, game_graphics::snake_node_height, snake_node::direction_e::UP, graphics_utils::palette_e::RED);
+		//centre_y += game_graphics::snake_node_height;
 
 	}
 
