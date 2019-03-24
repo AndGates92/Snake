129c6285c56c608d3ea082e54920b6f16be8ab09
AndGate92 <mrandre.epfl@gmail.com>
2019-03-24 19:41:35 +0000

    Add obstacle list

---
+++ include/obstacle_list.h
@@ -0,0 +1,60 @@
+#ifndef OBSTACLE_LIST_H
+#define OBSTACLE_LIST_H
+/**
+ * @copyright
+ * @file obstacle_list.h
+ * @author Andrea Gianarda
+ * @date 24th March 2019
+ * @brief Obstacle List header file
+*/
+
+#include "graphics_utils.h"
+#include "obstacle.h"
+
+namespace obstacle_list {
+	/** @defgroup ObstacleListGroup Obstacle List Doxygen Group
+	 *  Obstacle List functions and classes
+	 *  @{
+	 */
+
+	class ObstacleList {
+		public:
+			// Constructor
+			ObstacleList(): head(nullptr) { LOG_INFO(logging::verb_level_e::LOW, "Obstacle list contructed") };
+
+			ObstacleList(const ObstacleList& copy): head(copy.head) { LOG_INFO(logging::verb_level_e::LOW, "Obstacle list copy contructor") };
+
+			// Destructor
+			~ObstacleList();
+
+			template <typename pixel_type>
+			void draw(pixel_type * & pixels, int & win_width);
+
+			void add_node(int centre_x, int centre_y, int obstacle_width, int obstacle_height, graphics_utils::palette_e obstacle_colour);
+
+			void print_info(logging::verb_level_e verbosity, std::string pretext);
+
+		protected:
+			void remove_node(obstacle::ObstacleNode * & node);
+
+		private:
+			obstacle::ObstacleNode * head;
+	};
+	/** @} */ // End of ObstacleListGroup group
+}
+
+template <typename pixel_type>
+void obstacle_list::ObstacleList::draw(pixel_type * & pixels, int & win_width) {
+	obstacle::ObstacleNode * obstacle_node = this->head;
+
+	while (obstacle_node != nullptr) {
+
+		obstacle_node->draw(pixels, win_width);
+
+		obstacle_node = obstacle_node->get_next();
+
+	}
+
+}
+
+#endif // OBSTACLE_LIST_H
---
+++ src/obstacle_list.cpp
@@ -0,0 +1,82 @@
+/**
+ * @copyright
+ * @file obstacle_list.cpp
+ * @author Andrea Gianarda
+ * @date 24th March 2019
+ * @brief Obstacle List function
+ */
+
+#include <iostream>
+#include <cmath>
+
+#include "game_graphics.h"
+#include "graphics_utils.h"
+#include "obstacle_list.h"
+#include "obstacle.h"
+#include "logging.h"
+
+using namespace std;
+using namespace logging;
+using namespace obstacle_list;
+using namespace obstacle;
+using namespace graphics_utils;
+using namespace game_graphics;
+
+obstacle_list::ObstacleList::~ObstacleList() {
+
+	std::string pretext ("Obstacle List Destructor");
+	this->print_info(logging::verb_level_e::LOW, pretext);
+
+	obstacle::ObstacleNode * node_ptr = this->head;
+	while (this->head != nullptr) {
+		node_ptr = this->head;
+		node_ptr->print_info(logging::verb_level_e::LOW, pretext);
+		this->head = this->head->get_next();
+		node_ptr->~ObstacleNode();
+	}
+	LOG_INFO(logging::verb_level_e::HIGH, "Obstacle list destroyed");
+
+}
+
+
+void obstacle_list::ObstacleList::add_node(int centre_x, int centre_y, int obstacle_width, int obstacle_height, graphics_utils::palette_e obstacle_colour) {
+
+	LOG_INFO(logging::verb_level_e::LOW, "[Add Node] Centre coordinares: (X ", centre_x, ", Y ", centre_y, "), width ", obstacle_width, ", height ", obstacle_height, " colour ", obstacle_colour, ".");
+	obstacle::ObstacleNode * new_obstacle = new obstacle::ObstacleNode(centre_x, centre_y, obstacle_width, obstacle_height, obstacle_colour);
+
+	new_obstacle->set_prev(nullptr);
+	new_obstacle->set_next(this->head);
+	this->head->set_prev(new_obstacle);
+	this->head = new_obstacle;
+
+}
+
+void obstacle_list::ObstacleList::remove_node(obstacle::ObstacleNode * & node) {
+	obstacle::ObstacleNode * node_saved = node;
+
+	if (node == this->head) {
+		// Move head pointer as obstacle list to delete is the head
+		if (node->get_next() == nullptr) {
+			this->head = nullptr;
+		} else {
+			this->head = this->head->get_next();
+			this->head->get_prev() = nullptr;
+		}
+	} else {
+		node->get_prev()->get_next() = node->get_next();
+		if (node->get_next() != nullptr) {
+			node->get_next()->get_prev() = node->get_prev();
+		}
+	}
+
+	node_saved->~ObstacleNode();
+}
+
+void obstacle_list::ObstacleList::print_info(logging::verb_level_e verbosity, std::string pretext) {
+	obstacle::ObstacleNode * obstacle_list = this->head;
+
+	while (obstacle_list != nullptr) {
+		obstacle_list->print_info(verbosity, pretext);
+		obstacle_list = obstacle_list->get_next();
+	}
+}
