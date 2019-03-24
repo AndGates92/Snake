f5c25e51d16e0e5b684ee2416ea0add1ab40d963
AndGate92 <mrandre.epfl@gmail.com>
2019-03-24 14:27:01 +0000

    Move move function close to other SnakeUnit functions

--- src/snake_node.cpp
+++ src/snake_node.cpp
@@ -83,6 +83,36 @@ void snake_node::SnakeUnit::set_colour(graphics_utils::palette_e new_colour) {
 }
 
 // ================================================================
+// Move function
+// ================================================================
+void snake_node::SnakeUnit::move(int speed, int win_width, int win_height) {
+
+	switch (this->direction) {
+		case snake_node::direction_e::RIGHT:
+			this->x_centre = (this->x_centre + speed) % win_width;
+			break;
+		case snake_node::direction_e::LEFT:
+			this->x_centre = (this->x_centre - speed) % win_width;
+			if (this->x_centre < 0) {
+				this->x_centre = win_width;
+			}
+			break;
+		case snake_node::direction_e::UP:
+			this->y_centre = (this->y_centre + speed) % win_height;
+			break;
+		case snake_node::direction_e::DOWN:
+			this->y_centre = (this->y_centre - speed) % win_height;
+			if (this->y_centre < 0) {
+				this->y_centre = win_height;
+			}
+			break;
+		default:
+			LOG_ERROR("Unknown direction");
+			break;
+	}
+}
+
+// ================================================================
 // Overload << operator for direction_e
 // ================================================================
 std::ostream& snake_node::operator<< (std::ostream& os, snake_node::direction_e direction) {
@@ -144,32 +174,5 @@ void snake_node::SnakeNode::set_prev(snake_node::SnakeNode * prev_ptr) {
 
 void snake_node::SnakeNode::print_info(logging::verb_level_e verbosity, std::string pretext) {
 	SnakeUnit::print_info(verbosity, pretext);
- 	LOG_INFO(verbosity, "[", pretext, "] Pointer addresses: next ", this->next, " previous " , this->prev, ".");
-}
-
-void snake_node::SnakeUnit::move(int speed, int win_width, int win_height) {
-
-	switch (this->direction) {
-		case snake_node::direction_e::RIGHT:
-			this->x_centre = (this->x_centre + speed) % win_width;
-			break;
-		case snake_node::direction_e::LEFT:
-			this->x_centre = (this->x_centre - speed) % win_width;
-			if (this->x_centre < 0) {
-				this->x_centre = win_width;
-			}
-			break;
-		case snake_node::direction_e::UP:
-			this->y_centre = (this->y_centre + speed) % win_height;
-			break;
-		case snake_node::direction_e::DOWN:
-			this->y_centre = (this->y_centre - speed) % win_height;
-			if (this->y_centre < 0) {
-				this->y_centre = win_height;
-			}
-			break;
-		default:
-			LOG_ERROR("Unknown direction");
-			break;
-	}
+	LOG_INFO(verbosity, "[", pretext, "] Pointer addresses: next ", this->next, " previous " , this->prev, ".");
 }
