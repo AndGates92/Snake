e903f21c17881f0f9e8be5ef1b1bd5e1fc0d1b70
Andrea Gianarda <mrandre.epfl@gmail.com>
2019-05-02 22:47:27 +0100

    Change background colour when creating windows

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -46,8 +46,8 @@ void graphics::init_graphics(int argc, char** argv) {
 }
 
 void graphics::add_graphics() {
-	game_win_id = graphics_utils::win_node_add("snake", 1100, 450, 100, 50, menu::menu_game, menu::menu_game_items, game_graphics::wrapper_game_cb, colours::palette_e::BLACK);
-	stat_win_id = graphics_utils::win_node_add("stat",  200,  50, 1500, 50, menu::menu_stat, menu::menu_stat_items, stat_graphics::wrapper_stat_cb, colours::palette_e::BLACK);
+	game_win_id = graphics_utils::win_node_add("snake", 1100, 450, 100, 50, menu::menu_game, menu::menu_game_items, game_graphics::wrapper_game_cb, colours::palette_e::PURPLE);
+	stat_win_id = graphics_utils::win_node_add("stat",  200,  50, 1500, 50, menu::menu_stat, menu::menu_stat_items, stat_graphics::wrapper_stat_cb, colours::palette_e::BROWN);
 }
 
 void graphics::start_game() {
